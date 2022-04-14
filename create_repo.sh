3781a5e2982e23c921b9e96300898850cd935932
jordan matos <68743706+jstarfox@users.noreply.github.com>
2022-04-14 14:32:52 -0400

    Delete create_repo.sh

--- create_repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws configure --access-key AKIAVO33MFIKK2T7O3E3 \
---secret-key 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 --region us-east-1
-
-aws ecr create-repository --repository-name jenkins-cicd 
-
