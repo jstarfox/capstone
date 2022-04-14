dec8659bc323ee50219828d8c9f722435495a064
jordan matos <68743706+jstarfox@users.noreply.github.com>
2022-04-14 11:52:07 -0400

    Update create_repo.sh

--- create_repo.sh
+++ create_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws configure --access-key --region us-east-1 $AWS_ACCESS_KEY_ID \
---secret-key $AWS_SECRET_ACCESS_KEY \
+aws configure --access-key AKIAVO33MFIKK2T7O3E3 \
+--secret-key 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 --region us-east-1
 
 aws ecr create-repository --repository-name jenkins-cicd 
 
